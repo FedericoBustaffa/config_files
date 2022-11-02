cf0614e03b8641648f5f0ecfab935354f9955113
FedericoBustaffa <kiko.bustaffa@gmail.com>
2022-11-02 15:07:54 +0100

    win flag

--- starship/Microsoft.PowerShell_profile.ps1
+++ starship/Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,5 @@
 ﻿$ENV:STARSHIP_CONFIG = "$HOME\.config\prompt\starship\starship.toml"
-$ENV:STARSHIP_DISTRO = "者 win"
+$ENV:STARSHIP_DISTRO = "者 "
 $ENV:STARSHIP_CACHE = "$HOME\AppData\Local\Temp"
 
 Invoke-Expression (&starship init powershell)
