c3091860710be88bf06c46ad3ee733d2bfdf94e4
FedericoBustaffa <kiko.bustaffa@gmail.com>
2022-11-02 14:50:44 +0100

    powershell

---
+++ starship/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,5 @@
+﻿$ENV:STARSHIP_CONFIG = "$HOME\.config\prompt\starship\starship.toml"
+$ENV:STARSHIP_DISTRO = "win"
+$ENV:STARSHIP_CACHE = "$HOME\AppData\Local\Temp"
+
+Invoke-Expression (&starship init powershell)
